55d4f7deea58584f99bd3cba2f3ee9449ab02d89
Bella Vartanyan <BellaVartanyan@rentalcars.com>
2019-08-28 17:56:00 +0100

    trying to push

---
+++ scripts/german_credit_bella.py
@@ -0,0 +1,14 @@
+#Bella's Python file for German Credit Kaggle
+import kaggle 
+kaggle.api.authenticate()
+
+
+#kaggle.api.dataset_download_files('The_name_of_the_dataset', path='the_path_you_want_to_download_the_files_to', unzip=True)
+
+
+
+
+
+
+##########CRAP
+# cd /c/Users/vartanyanb/python_projects/german_credit
--- scripts/german_credit_master.py
+++ scripts/german_credit_master.py
@@ -1,1 +1,5 @@
 #Master Python file for German Credit Kaggle
+import kaggle 
+kaggle.api.authenticate()
+
+#kaggle.api.dataset_download_files('The_name_of_the_dataset', path='the_path_you_want_to_download_the_files_to', unzip=True)
